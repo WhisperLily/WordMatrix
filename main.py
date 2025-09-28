2dcb496ae2d2dcd2857df9c8636a36f28d2e6c9f
WhisperLily <schola.knj@gmail.com>
2025-09-28 16:06:56 +0100

    created html, css, fastapi for WM - 1

---
+++ main.py
@@ -0,0 +1,21 @@
+import WordMatrix
+from fastapi import FastAPI, Depends, HTTPException, status, Response, Request
+from fastapi import WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory='.'), name="static")
+
+@app.get("/")
+def get_HomePage():
+    with open("/static/index.html", "r") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
+
+
+@app.get("word")   #getting game status label; how many words left to play
+@app.get("score")   #getting status label; how many games won 
+@app.get("attempts")    #getting status label; how many attempts left 
+@app.get("scrmabled_word")      #getting the scrambled letters
+@app.get("hint")       #click hint button to show word meaning
